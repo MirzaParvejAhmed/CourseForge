8afc82b1addea3e1df79d5e8224e1b8bf7c88911
Abhishek singh <abhisheksinghmd2202@gmail.com>
2025-02-02 12:19:17 +0530

    Update passwordupdate.js

--- server/mail/template/passwordupdate.js
+++ server/mail/template/passwordupdate.js
@@ -55,7 +55,7 @@ exports.passwordUpdated = (email, name) => {
     
     <body>
         <div class="container">
-            // <a href="https://codeplay-edtech-project.vercel.app"><img class="logo"
+            // <a href="https://techshala.vercel.app/><img class="logo"
             //         src="https://res.cloudinary.com/dazj8oisj/image/upload/v1738360793/abhi_logo_1_yhursc.png" alt="TechShala Logo"></a>
             <div class="message">Password Update Confirmation</div>
             <div class="body">
